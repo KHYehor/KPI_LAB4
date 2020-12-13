a601b99c337f5b637ee61bedc039aabc3ae76658
ykh2k <khilchenko.yehor@gmail.com>
2020-12-13 18:13:52 +0200

    feat: save progress

---
+++ KPI_LAB4/TestBinaryFlag_FileWorker/TestIntegration.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Xunit;
+using Xunit.Abstractions;
+using IIG.BinaryFlag;
+using IIG.FileWorker;
+
+namespace KPI_LAB4.TestBinaryFlag_FileWorker
+{
+    public class TestIntegration : IDisposable
+    {
+        private static string AbsolutePath = "/Applications/github/KPI_LAB4/KPI_LAB4/TestBinaryFlag_FileWorker";
+
+        private static string InitDirectory = "initDirectory";
+        private static string InitFile = "initFile.txt";
+        private static string InitAbsolutePath = AbsolutePath + "/" + InitDirectory + "/" + InitFile;
+
+        private static string TestDirectory = "testDirectory";
+        private static string TestFile = "testFile.txt";
+
+        private MultipleBinaryFlag _MBF;
+        private ulong SIZE = 5;
+
+        public TestIntegration()
+        {
+            _MBF = new MultipleBinaryFlag(SIZE);
+        }
+
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+        }
+
+
+        // 1. Create directory
+        // 2. Copy file
+        // 3. Get file name via BaseFileWorker
+        // 4. Get file text via BaseFileWorker
+        // 5. Get MultipleBinaryFlag flag (true)
+        // 6. Reset Flag to false and get status
+        // 7. Set Flag to true and get status
+        // 8. Write 1st flag to file and read it
+        // 9. Write 2nd flag to file and read it
+        // 10. Write 3rd flag to file and read it
+
+        [Fact]
+        public void TestSaveBinaryFlagToFileSuccess()
+        {
+            string targetPath = BaseFileWorker.MkDir(TestDirectory);
+            // 1. Test if dir was crated
+            Assert.NotNull(targetPath);
+            bool status1 = BaseFileWorker.TryCopy(InitAbsolutePath, targetPath + "/" + TestFile, true, 10);
+            // 2. Test if file was copied and previous dir was created
+            Assert.True(status1);
+            string fileName = BaseFileWorker.GetFileName(targetPath + "/" + TestFile);
+            // 3. Test if file system if it was created
+            Assert.Equal(TestFile, fileName);
+            string fileText = BaseFileWorker.ReadAll(targetPath + "/" + TestFile);
+            // 4. Test if file was read right
+            Assert.Equal("some test text", fileText);
+            bool flag1 = _MBF.GetFlag();
+            // 5. Test flag status
+            Assert.True(flag1);
+            _MBF.ResetFlag(SIZE - 2);
+            bool flag2 = _MBF.GetFlag();
+            // 6. Test flag status after reset
+            Assert.False(flag2);
+            _MBF.SetFlag(SIZE - 2);
+            bool flag3 = _MBF.GetFlag();
+            // 7. Test flag status after set
+            Assert.True(flag3);
+            bool status2 = BaseFileWorker.TryWrite(flag1.ToString(), targetPath + "/" + TestFile, 10);
+            // Check if previous operation was successful
+            Assert.True(status2);
+            string[] lines1 = BaseFileWorker.ReadLines(targetPath + "/" + TestFile);
+            // 8. Check if writing to file was successful
+            Assert.Equal(lines1[0], true.ToString());
+            bool status3 = BaseFileWorker.Write(flag2.ToString(), targetPath + "/" + TestFile);
+            // Check if previous operation was successful
+            Assert.True(status3);
+            string[] lines2 = BaseFileWorker.ReadLines(targetPath + "/" + TestFile);
+            // 9. Check if writing to file was successful
+            Assert.Equal(lines2[0], false.ToString());
+            bool status4 = BaseFileWorker.Write(flag3.ToString(), targetPath + "/" + TestFile);
+            // Check if previous operation was successful
+            Assert.True(status4);
+            string[] lines3 = BaseFileWorker.ReadLines(targetPath + "/" + TestFile);
+            // 10. Check if writing to file was successful
+            Assert.Equal(lines3[0], true.ToString());
+        }
+    }
+}
---
+++ KPI_LAB4/TestPasswordHashing_AuthDataBase/TestIntegration.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Xunit;
+using Xunit.Abstractions;
+using IIG.PasswordHashingUtils;
+using IIG.CoSFE.DatabaseUtils;
+namespace KPI_LAB4.TestPasswordHashing_AuthDataBase
+{
+    public class TestIntegration : IDisposable
+    {
+        private static string server = @"127.0.0.1";
+        private static string database = @"IIG.CoSWE.AuthDB";
+        private static bool isTrusted = true;
+        private static string login1 = @"coswe";
+        private static string login2 = "login2";
+        private static string login3 = "login3";
+        private static string password1 = @"L}EjpfCgru9X@GLj";
+        private static string password2 = "password2";
+        private static string password3 = "password3";
+        private static int connectionTimeOut = 15;
+
+        private readonly ITestOutputHelper output;
+
+        public TestIntegration(ITestOutputHelper output)
+        {
+            PasswordHasher.Init("salt", 23432);
+            this.output = output;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        [Fact]
+        public void TestSuccessfulCase()
+        {
+            string password = PasswordHasher.GetHash("password");
+            // 1. Check if password hashes the same way
+            Assert.Equal(password, PasswordHasher.GetHash("password"));
+            AuthDatabaseUtils connection = new AuthDatabaseUtils(server, database, isTrusted, login1, password1, connectionTimeOut);
+            // 2.
+            output.WriteLine(connection.getStatus().ToString());
+            // Assert.True(connection.getStatus());
+            bool status1 = connection.AddCredentials(login2, password2);
+            output.WriteLine(status1.ToString());
+            // 3.Check status of adding credentials
+            //Assert.True(status1);
+            //bool status2 = connection.CheckCredentials(login2, password2);
+            //Assert.True(status2);
+            //bool status3 = connection.UpdateCredentials(login2, password2, login3, password3);
+            //Assert.True(status3);
+            //bool status4 = connection.CheckCredentials(login3, password3);
+            //Assert.True(status4);
+            //bool status5 = connection.DeleteCredentials(login3, password3);
+            //Assert.True(status5);
+            //bool status6 = connection.CheckCredentials(login3, password3);
+            //Assert.False(status6);
+        }
+
+    }
+}
