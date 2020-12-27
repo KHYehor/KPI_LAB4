efc8699d6f470e151dcc1779c36f423f6de3108e
ykh2k <khilchenko.yehor@gmail.com>
2020-12-27 15:25:12 +0200

    feat: update comments

--- KPI_LAB4/TestPasswordHashing_AuthDataBase/TestIntegration.cs
+++ KPI_LAB4/TestPasswordHashing_AuthDataBase/TestIntegration.cs
@@ -7,7 +7,7 @@ namespace KPI_LAB4.TestPasswordHashing_AuthDataBase
 {
     public class TestIntegration : IDisposable
     {
-        private static string server = @"192.168.0.110";
+        private static string server = @"127.0.0.1";
         private static string database = @"IIG.CoSWE.AuthDB";
         private static bool isTrusted = false;
         private static string login1 = @"sa";
@@ -17,8 +17,7 @@ namespace KPI_LAB4.TestPasswordHashing_AuthDataBase
         private static string password2 = "password2";
         private static string password3 = "password3";
         private static int connectionTimeOut = 10;
-
-        private readonly ITestOutputHelper output;
+        private ITestOutputHelper output;
 
         public TestIntegration(ITestOutputHelper output)
         {
@@ -30,6 +29,13 @@ namespace KPI_LAB4.TestPasswordHashing_AuthDataBase
         {
         }
 
+        // 1. Check if password hashes the same way
+        // 2. Check status of adding credentials
+        // 3. Check if credentials were saved
+        // 4. Check status
+        // 5. Check if credentials were updated
+        // 6. Check status
+        // 7. Check if credentials were deleted
         [Fact]
         public void TestSuccessfulCase()
         {
@@ -39,20 +45,23 @@ namespace KPI_LAB4.TestPasswordHashing_AuthDataBase
             // 1. Check if password hashes the same way
             Assert.Equal(password1Hashed, PasswordHasher.GetHash(password1));
             AuthDatabaseUtils connection = new AuthDatabaseUtils(server, database, isTrusted, login1, password1, connectionTimeOut);
-            // 2.
-            // Assert.True(connection.getStatus());
             bool status1 = connection.AddCredentials(login2, password2Hashed);
-            // 3.Check status of adding credentials
+            // 2. Check status of adding credentials
             Assert.True(status1);
             bool status2 = connection.CheckCredentials(login2, password2Hashed);
+            // 3. Check if credentials were saved
             Assert.True(status2);
             bool status3 = connection.UpdateCredentials(login2, password2Hashed, login3, password3Hashed);
+            // 4. Check status
             Assert.True(status3);
             bool status4 = connection.CheckCredentials(login3, password3Hashed);
+            // 5. Check if credentials were updated
             Assert.True(status4);
             bool status5 = connection.DeleteCredentials(login3, password3Hashed);
+            // 6. Check status
             Assert.True(status5);
             bool status6 = connection.CheckCredentials(login3, password3Hashed);
+            // 7. Check if credentials were deleted
             Assert.False(status6);
         }
 
